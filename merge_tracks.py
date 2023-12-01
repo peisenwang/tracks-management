58d0832a27e04a44bed39470578aac1343021941
Peisen Wang <wpeisen@gmail.com>
2023-12-01 13:35:26 +0800

    Merge and simplify tracks

---
+++ merge_tracks.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+import argparse
+
+import gpxpy
+from tqdm import tqdm
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Merge multi gpx files into one with simplification')
+    parser.add_argument('tracks', nargs='+', help='Paths to gpx file to merge')
+    parser.add_argument(
+        '-d', '--dist', default=10, help='Distance for simplying tracks')
+    parser.add_argument('-o', '--out', required=True, help='Output gpx path')
+    parser.add_argument(
+        '--nameless', action='store_true',
+        help='Don\'t name tracks for better visualization in google earth')
+    args = parser.parse_args()
+
+    # assert len(args.tracks) > 1, 'Need more than one gpx files to merge '
+
+    tracks = []
+    for path in tqdm(args.tracks):
+        with open(path) as f:
+            gpx = gpxpy.parse(f)
+
+        assert len(gpx.tracks) == 1
+        track = gpx.tracks[0]
+
+        # Avoid wired bug when "osmand:" get replaced by
+        # "{https://osmand.net}", causing output track un-loadable.
+        for s in track.segments:
+            for p in s.points:
+                p.extensions = []
+
+        if args.dist > 0:
+            track.simplify(args.dist)
+
+        if args.nameless:
+            name = ''
+        else:
+            name, _ = os.path.splitext(os.path.basename(path))
+        track.name = name
+        tracks.append(track)
+
+    gpx = gpxpy.gpx.GPX()
+    gpx.tracks = tracks
+
+    with open(args.out, 'w') as f:
+        f.write(gpx.to_xml())
